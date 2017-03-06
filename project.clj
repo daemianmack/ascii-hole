d19c82d8bbb89b947f3a3c685daf4a4276bef6e6
Daemian Mack <daemianmack@gmail.com>
2017-03-06 08:13:17 -0500

    Prepare for CLJC port.
    - Also bump clojure, core.async versions.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/daemianmack/ascii-hole"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/core.async "0.3.441"]
                  [jline "2.11"]
                  [table "0.4.0"]]
   :profiles {:dev {:source-paths ["dev"]
