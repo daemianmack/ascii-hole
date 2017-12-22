cb06f482e54ea96438dcef61052734a33be3c555
Daemian Mack <daemianmack@gmail.com>
2017-12-22 12:59:07 -0500

    Bump clj, core.async versions.
    - Ran afoul of https://github.com/clojure/core.async/commit/2f87bc7c7d10cb7b0baafc86e08489d58fa87424#diff-5c087e8e400be45f4d03e0a618ef9d46

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ascii-hole "0.3.0"
+(defproject ascii-hole "0.3.1"
   :description "Let console keystrokes asynchronously trigger specified CLJ/CLJS functions."
   :url "https://github.com/daemianmack/ascii-hole"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/core.async "0.3.441"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.3.465"]
                  [jline "2.11"]]
   :profiles {:dev {:source-paths ["dev"]
                    :aliases {"run-dev" ["trampoline" "run" "-m" "dev.user/-main"]}}})
